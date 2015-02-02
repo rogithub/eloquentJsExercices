2e841c946076ca35c511374cf26912a8ffc2cdb3
roubuntu <correo.rodrigo@gmail.com>
2015-02-02 16:15:38 -0600

    range step

--- chapter4-DataStructures.js
+++ chapter4-DataStructures.js
@@ -1,17 +1,29 @@
 'use strict';
 
-let range = function(start, end){
+let range = function(start, end, step) {
   let arr = [];  
-  for (let i = start; i <= end; i++){
-    arr.push(i);        
+  step = step || 1;  
+     
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+  } else {
+    // (5, 2, -1) [5, 4, 3, 2]
+    for(let i = start; i >= end; i += step) {
+      arr.push(i);
+    }  
   }
+
   return arr;
 }
 
 let sum = function(arr) {
   var counter = 0;
+
   for(let i = 0; i < arr.length; i++)
     counter += arr[i];
+
   return counter;
 }
 
--- test/chapter4-DataStructures-test.js
+++ test/chapter4-DataStructures-test.js
@@ -7,3 +7,9 @@ exports.sumRangeTest = function(test) {
   test.done();
 }
 
+exports.rangeStepTest = function(test) {
+  test.expect(2);
+  test.deepEqual(f.range(1, 10, 2), [1, 3, 5, 7, 9]);
+  test.deepEqual(f.range(5, 2, -1), [5, 4, 3, 2]);
+  test.done();
+}
