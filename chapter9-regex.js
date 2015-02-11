51f5a7d5ca4d71568717ce3dc0c2cef28448b3fd
Rodrigo <correo.rodrigo@gmail.com>
2015-02-11 22:06:10 +0000

    regex numbers tests

--- chapter9-regex.js
+++ chapter9-regex.js
@@ -1,7 +1,19 @@
 'use strict';
 
+let isJsNumber = function(text) {
+    let number = /^(-|\+)?\d+(\.)?$/;
+    if (number.test(text)) return true;
+    
+    let exponent = /^(\d.)?\d+[eE]{1}(-|\+)?\d+$/;
+    if (exponent.test(text)) return true;
+    
+    let decimal = /^(-|\+)?(\d+)?[.]{1}\d+$/
+    return decimal.test(text);
+};
+
 let singleToDoubleQuotes = function(text) {
-    return text.replace(/'\b\w+\b'/g, "\"");
+    return text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2');
 };
 
 exports.singleToDoubleQuotes = singleToDoubleQuotes;
+exports.isJsNumber = isJsNumber;
--- test/chapter9-regex-test.js
+++ test/chapter9-regex-test.js
@@ -1,6 +1,33 @@
 'use strict';
 const m = require("./../chapter9-regex.js");
-/*
+
+exports.isJsNumberTest = function(test) {
+    test.expect(32);
+    let validNumbers = ["1", "-1", "1.0", "-1.0", 
+    "-1.509", "-1.509", "10e-20", "33e-4", ".5", "-0.5", 
+    "-.78", "+0.20", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
+    "1e+12"];
+    
+    for(let i = 0; i < validNumbers.length; i++) {
+        let number = validNumbers[i];
+        let actual = m.isJsNumber(number);
+        
+        test.equal(actual, true, "is number: " + number);
+    }
+    
+    let invalidNumbers = ["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5",
+    ".", "120c", "-44dias", "a10b", "e-10.0", "12k3"]; 
+    
+    for(let i = 0; i < invalidNumbers.length; i++) {
+        let number = invalidNumbers[i];
+        let actual = m.isJsNumber(number);
+        
+        test.equal(actual, false, "is not a number: " + number);
+    }
+    
+    test.done();
+};
+
 exports.singleToDoubleQuotesTest = function(test) {
     test.expect(1);
     
@@ -11,4 +38,4 @@ exports.singleToDoubleQuotesTest = function(test) {
     test.equal(actual, expected);
     
     test.done();
-}*/
+};
